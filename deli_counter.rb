bdb8ee3943107145993a8691e90173265ce47d31
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-04 03:00:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(deli)
+  if deli.length <1
+    return "The line is currently empty."
+  else
+    return "The line is currently: #{array(0..-1).join} and #{array[-1]}"
+  end
+end
+
+def take_a_number(array,name)
+  
+end
+
+def now_serving(array)
+end
