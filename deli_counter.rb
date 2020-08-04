5cfd5e5599276f350f6cec221345589a751acb91
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-04 03:07:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(deli)
   if deli.length >=1 
-    deli_line = deli.collect{|name| "#{deli[name]}. name "
+    deli_line = deli.collect{|name| "#{deli[name]}. name "}
     puts "The line is currently: #{deli_line}"
   else 
     puts "The line is currently empty."
